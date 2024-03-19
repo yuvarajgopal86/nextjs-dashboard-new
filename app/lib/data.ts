3de6d5f5eb6605c107080d2e5d892d173dad2e12
yuvarajgopal86 <g.yuvarajme@gmail.com>
2024-03-18 18:53:48 -0700

    chapter7

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,6 +10,14 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
+// Fetch the last 5 invoices, sorted by date
+const data = await sql<LatestInvoiceRaw>`
+  SELECT invoices.amount, customers.name, customers.image_url, customers.email
+  FROM invoices
+  JOIN customers ON invoices.customer_id = customers.id
+  ORDER BY invoices.date DESC
+  LIMIT 5`;
+
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
